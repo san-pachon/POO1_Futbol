231732ce82922946267b5d17030edc0bbb8d04f0
sigmotoa <si.galvismotoa@gmail.com>
2025-04-28 16:34:22 -0500

    Creacion de jugador y equipo de un solo jugador.

---
+++ src/Player.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Player {
+
+    //Atributos de la clase
+
+    public String name;
+    public int dorsal;
+    public String team;
+    public String position;
+    public double weight;
+    public double height;
+    public LocalDate birthday;
+    public String nationality;
+    public boolean isActive;
+
+
+}
---
+++ src/Team.java
@@ -0,0 +1,11 @@
+public class Team {
+
+    public int id;
+    public String name;
+    public Player dt;
+    public Player[] players = new Player[1];
+    public String city;
+    public String stadium;
+
+
+}
